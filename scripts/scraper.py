957fa33e5dde36fd52cdf3fe1a75a0e3c4d33875
CrispySalt <jacksepticeyee@gmail.com>
2023-05-10 07:39:22 +0700

    finished tokopedia scrape, wip lazada scrape.

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -7,20 +7,31 @@ def scrapeTokopedia(name):
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36', 
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", 
     "Accept-Language": "en-US,en;q=0.9" 
-}
+    }
     response = requests.get(url,headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
-    price_divs = soup.find_all('div', {'id': 'spnSRPProdPrice'})
-    #ini hanya dapet price, blm dapet nama, gambar(kalo perlu)
 
-def scrapeShopee(name):
+    product_div = soup.find_all('div', {'data-testid': 'master-product-card'})
+    data_list = [
+    {
+        'link': product.find('a', {'class': 'pcv3__info-content css-gwkf0u'})['href'],
+        'img_src': product.find('img', {'class': 'css-1q90pod'})['src'],
+        'name': product.find('div', {'data-testid': 'spnSRPProdName'}).text.strip(),
+        'price': product.find('div', {'data-testid': 'spnSRPProdPrice'}).text.strip()
+    }
+    for product in product_div
+    ]
+    return data_list
+
+def scrapeShopee(name):#might be impossible??? idk sumpah keluar garbage value terus
     url = 'https://shopee.co.id/search?keyword='+name
     headers = { 
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36', 
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", 
     "Accept-Language": "en-US,en;q=0.9" 
-}
+    }
     response = requests.get(url,headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
     return
 
 def scrapeLazada(name):
@@ -29,7 +40,7 @@ def scrapeLazada(name):
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36', 
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", 
     "Accept-Language": "en-US,en;q=0.9" 
-}
+    }
     response = requests.get(url,headers=headers)
     return
 
@@ -42,9 +53,9 @@ def dailyScrape():
     #Node: Name need to be pre-processed so that any space ' ' changed into '+', atleast on tokopedia. other idk
     scrapelist =['bakso','kalkulator']#temp array
     for i in scrapelist:
-        scrapeLazada(i)
-        scrapeShopee(i)
-        scrapeTokopedia(i)
+        lazada_list=scrapeLazada(i)
+        shopee_list=scrapeShopee(i)
+        tokopedia_list=scrapeTokopedia(i)
     return
 
 

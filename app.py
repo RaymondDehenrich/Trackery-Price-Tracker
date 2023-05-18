863793c8345f9f3a7a3b0e88b58a19705d8b7ae5
Vincentius Vincentius <vincentius_vin@yahoo.com>
2023-05-18 14:12:47 +0700

    Trim yang bukan bagian gw

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 from flask import Flask, render_template,jsonify
-import schedule
-import time
-from scripts.linear_reg_pred import *
-from scripts.scraper import *
 
 app = Flask(__name__)
 
@@ -10,15 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-schedule.every().day.at("12:00").do(dailyScrape)
-
-def start_scheduler():
-    while True:
-        schedule.run_pending()
-        time.sleep(60)
-
 if __name__ == "__main__":
-    import threading
-    scheduler_thread = threading.Thread(target=start_scheduler)
-    scheduler_thread.start()
-    app.run(host='0.0.0.0',debug=True)
+    app.run(debug=True)

06a5b04921b197dbd7f31981524dbe3f75120002
CrispySalt <jacksepticeyee@gmail.com>
2023-05-21 00:11:52 +0700

    finished home, rename pic, recolor navbar/footer

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template,jsonify
+from flask import Flask, render_template,jsonify,request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
+
+#for /search route, might want to have data = request.form.get('home-search'), 
+# so that user can directly start search on home and redirect to /search
 
 if __name__ == "__main__":
     app.run(debug=True)

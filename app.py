98d133c12c3ce2a260b9045e8eb78a382e4a2eb2
CrispySalt <jacksepticeyee@gmail.com>
2023-05-21 21:09:35 +0700

    added starting foundation for history page

--- app.py
+++ app.py
@@ -12,6 +12,34 @@ def login():
     page_name="login"
     return render_template('login.html',page_name=page_name)
 
+#Will need integration with user, for example, the link should be like /<username>/history, for now will be a temporary link route
+@app.route('/history',methods=['GET'])
+def history():
+    #added temporary list for history, should be integrated with database
+    
+    #ASSUMPTION
+    #1. only have 5 variable each.
+    #2. has already been sorted according to time.
+    #3. everything other than history-id is string.
+    history_list = [
+        {
+        'history-id':1,
+        'history-search':'bakso',
+        'average-price':'Rp.40.000',
+        'time': '13:20',
+        'date': '20/04/2023'
+        },
+        {
+        'history-id':2,
+        'history-search':'pangsit',
+        'average-price':'Rp.30.000',
+        'time': '13:40',
+        'date': '20/04/2025'
+        }
+    ]
+    return render_template('history.html',history_list=history_list)
+
+
 #for /search route, might want to have data = request.form.get('home-search'), 
 # so that user can directly start search on home and redirect to /search
 

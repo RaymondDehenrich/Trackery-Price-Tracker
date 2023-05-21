9493af43f569befcab455fc7cb13183b063354b6
CrispySalt <jacksepticeyee@gmail.com>
2023-05-21 20:54:02 +0700

    mostly cleaning up code and file structure

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    page_name="home"
+    return render_template('home.html',page_name=page_name)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    page_name="login"
+    return render_template('login.html',page_name=page_name)
 
 #for /search route, might want to have data = request.form.get('home-search'), 
 # so that user can directly start search on home and redirect to /search

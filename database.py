8e73541fbfb5c7627d542ffdc7f46641abc2dc6e
gagorus <91309997+gagorus@users.noreply.github.com>
2023-05-26 14:21:00 +0700

    Add files via upload

---
+++ database.py
@@ -0,0 +1,20 @@
+import sqlalchemy
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+db_string = "mysql+pymysql://csupqj8pv97jqs61vmv4:pscale_pw_Fkyc2aOFQIo2Ev8S04gnfyUj2jZtXDiGs54G8uKXWQM@aws.connect.psdb.cloud/trackery?charset=utf8mb4"
+
+engine = create_engine("mysql+pymysql://csupqj8pv97jqs61vmv4:pscale_pw_Fkyc2aOFQIo2Ev8S04gnfyUj2jZtXDiGs54G8uKXWQM@aws.connect.psdb.cloud/trackery?charset=utf8mb4", 
+    connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem",
+        }
+    })
+with engine.connect() as conn:
+    result = conn.execute(text("select * from User")) 
+    result_dicts = []
+    for row in result.all():
+        result_dicts.append(row._mapping)
+    print(result_dicts)
+
+
+

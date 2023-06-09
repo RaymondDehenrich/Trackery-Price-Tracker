ab1d8abcf69c0160eabc79276c93268b3e4eb6d8
gagorus <91309997+gagorus@users.noreply.github.com>
2023-06-09 23:44:48 +0700

    predict next day with timeseries

--- scripts/linear_reg_pred.py
+++ scripts/linear_reg_pred.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from datetime import date
 from sklearn.linear_model import LinearRegression
-
+import matplotlib.pyplot as plt
 #-1 return means empty array
 #-10 return means no data or less than 5 data is available
 #-404 return means dataframe conversion error
@@ -25,23 +25,58 @@ def lin_reg(data):
     if df.shape[0]<5:
         return -10
     
-
-    X = df['Date'].values.reshape(-1, 1)
-    y = df['Price'].values.reshape(-1, 1)
-
+    #BUAT LINREG BIASA
+    # df.insert(0,"Time",df.index)
+   
+    # X = df['Time'].values.reshape(-1, 1)
+    # y = df['Price'].values.reshape(-1, 1)
     
-    split = int(0.8 * len(X))
-    X_train = X[:split]
-    X_test = X[split:]
-    y_train = y[:split]
-    y_test = y[split:]
+    # split = int(0.8 * len(X))
+
+    # X_train = X[:split]
+    # X_test = X[split:]
+    # y_train = y[:split]
+    # y_test = y[split:]
+    # model = LinearRegression()
+    # model.fit(X_train,y_train)
+
+    # #model test here, not required tbh
+
+    # next_day = X_test[-1]+1
+    # predicted_price = model.predict([next_day])[0][0]
+
+    #buat timeseries
     
+    past_days = 3
+    df2 = df[['Price']]
+   
+    def TimeLagTransform(data, past_days):
+        cols = data.columns
+        for i in reversed(range(past_days+1)):
+            for j in cols:
+                if i>0:
+                    data['%s(t-%d)'%(j,i)] = data[j].shift(periods=i)
+                else:
+                    data[f"{j}-Target"] = data[j].shift(periods=i)
+        return data
+    df2 = TimeLagTransform(data=df2.copy(), past_days=past_days).drop(columns=df2.columns, axis=1).dropna()
+    split = int(len(df2)-1)
+    train = df2.iloc[:split]
+    test = df2.iloc[split:]
+    X_train, y_train = train.iloc[:, :-1], train.iloc[:, -1:]
+    X_test, y_test = test.iloc[:, :-1], test.iloc[:, -1:]
+    nextday=pd.DataFrame(X_test,columns=['Price(t-3)','Price(t-2)'])
+    nextday.insert(2,"Price(t-1)",y_test)
+    print(nextday)
     model = LinearRegression()
     model.fit(X_train,y_train)
+    predicted_price = model.predict(nextday)[0][0]
 
-    #model test here, not required tbh
+    plt.figure(figsize=(10,6))
+    plt.title('Last 7 day price')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.plot(df['Date'][3:], df['Price'][3:],marker = ".")
+    plt.savefig('static/images/plot.png')
 
-    next_day = X_test[-1]+1
-    predicted_price = model.predict([[next_day]])[0][0]
-    
     return predicted_price
